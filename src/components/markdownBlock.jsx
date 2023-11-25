92c16410ae38b5d353c61139e8fba8d10308e3bf
allen3325 <00857146@email.ntou.edu.tw>
2023-11-25 14:17:57 +0000

    feat: using polling to get data.

--- src/components/markdownBlock.jsx
+++ src/components/markdownBlock.jsx
@@ -8,16 +8,19 @@ import Stack from "react-bootstrap/Stack";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { micromark } from 'micromark'
-import { gfmTable, gfmTableHtml } from 'micromark-extension-gfm-table'
+import { micromark } from "micromark";
+import { gfmTable, gfmTableHtml } from "micromark-extension-gfm-table";
 import "./loading.css";
-import { v1 as uuidv1 } from 'uuid';
+import { v1 as uuidv1 } from "uuid";
 
 function MarkdownBlocks() {
   const [keyword, setKeyword] = useState("");
   const [markdown, setMarkdown] = useState("");
   let today = new Date();
-  const [dateRange, setDateRange] = useState([new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000), new Date(today.getTime() - 1 * 24 * 60 * 60 * 1000)]);
+  const [dateRange, setDateRange] = useState([
+    new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000),
+    new Date(today.getTime() - 1 * 24 * 60 * 60 * 1000),
+  ]);
   const [startDate, endDate] = dateRange;
   let timer = 0;
 
@@ -27,73 +30,79 @@ function MarkdownBlocks() {
 
   // Count for delay request
   function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
   // Polling response from GPT's report
   async function pollingData(url) {
-    timer += 1
-    console.log("in pollingData, check timer.")
-    console.log("timer is ", timer)
-    if (timer > 2) {
-      return
+    console.log("in pollingData, check timer.");
+    timer += 1;
+    console.log("timer is ", timer);
+    if (timer > 6) {
+      console.log("timer > 6.");
+      setMarkdown("GPT Timeout.");
     } else {
-      try {
-        const response = await axios.get(url);
-        console.log('Response:', response.status, response.data);
-        return response;
-      } catch (error) {
-        // If received 404. Retrying in 10 seconds
-        if (error.response && error.response.status === 404) {
-          console.log('Received 404. Retrying in 10 seconds...');
-          await delay(10000); // 10000
-          return pollingData(url);
-        } else {
-          // Catch other error
-          console.error('Error:', error);
-          setMarkdown("# 發生錯誤")
-          throw error;
-        }
-      }
+      console.log("call check API.");
+      axios
+        .get(url)
+        .then((response) => {
+          console.log("call check API done.");
+          console.log("Response:", response);
+          setMarkdown(response.data.result);
+        })
+        .catch((error) => {
+          // If received 404. Retrying in 10 seconds
+          if (error.response && error.response.status === 404) {
+            console.log("Received 404. Retrying in 10 seconds...");
+            setTimeout(() => {
+              pollingData(url);
+            }, 10000); // Polling data per 10 seconds
+          } else {
+            // Catch other error
+            console.error("Error in polling:", error);
+            throw error;
+          }
+        });
     }
   }
 
-
   function submit() {
+    timer = 0;
     const uuidOfSession = uuidv1();
     console.log("Keyword is :", keyword);
     console.log("UUID is :", uuidOfSession);
-    let url = "http://127.0.0.1:8000/analyze/" + keyword + "?uuidOfSession=" + uuidOfSession + "&tag=%E6%96%B0%E8%81%9E&K=5&size=10000&start=" + startDate.getTime().toString().slice(0, -3) + "&end=" + endDate.getTime().toString().slice(0, -3)
+    // let url = "https://potato-elfbackend.nlpnchu.org/analyze/" + keyword + "?uuidOfSession=" + uuidOfSession + "&tag=%E6%96%B0%E8%81%9E&K=5&size=10000&start=" + startDate.getTime().toString().slice(0, -3) + "&end=" + endDate.getTime().toString().slice(0, -3)
+    let url =
+      "http://127.0.0.1:8000/analyze/" +
+      keyword +
+      "?uuidOfSession=" +
+      uuidOfSession +
+      "&tag=%E6%96%B0%E8%81%9E&K=5&size=10000&start=" +
+      startDate.getTime().toString().slice(0, -3) +
+      "&end=" +
+      endDate.getTime().toString().slice(0, -3);
     setMarkdown("loading");
 
     if (keyword == "test_for_NCHU_NLP_LAB") {
-      axios.post(url)
+      axios
+        .post(url)
         .then((res) => {
           setMarkdown(res.data);
         })
-        .catch(err => setMarkdown("# Server Error."))
+        .catch((err) => setMarkdown("# Server Error."));
     } else {
       // Don't wait response. Use polling.
-      axios.post(url)
-      console.log("Wait for 60 seconds.")
-      setTimeout(() => {
-        pollingData('http://127.0.0.1:8000/check/' + uuidOfSession)
-          .then(result => {
-            console.log("Get response by ", uuidOfSession);
-            setMarkdown(result.data);
-          })
-          .catch(err => {
-            console.log("error: ", err);
-            if (timer > 2) {
-              setMarkdown("# GPT Timeout.");
-            } else {
-              setMarkdown("# 發生錯誤")
-            }
-
-          });
-      }, 60000);
+      axios
+        .post(url)
+        .then(() => {
+          console.log("Wait for 60 seconds.");
+          setTimeout(() => {
+            // pollingData('https://potato-elfbackend.nlpnchu.org/check/' + uuidOfSession)
+            pollingData("http://127.0.0.1:8000/results/" + uuidOfSession);
+          }, 60000); // wait 60 seconds to polling
+        })
+        .catch((err) => console.log("error in analyze, ", err));
     }
-
   }
 
   return (
@@ -122,12 +131,19 @@ function MarkdownBlocks() {
 
       {markdown == "loading" ? (
         <div className="loader-container">
-          
-          <h3 style={{ textAlign: 'center', marginBottom: '1rem' }}><img className="YT" src="https://imgur.com/tonaxAB.png" alt="" />報表生成中，請稍等一至兩分鐘<img className="YT" src="https://imgur.com/tonaxAB.png" alt="" /></h3>
+          <h3 style={{ textAlign: "center", marginBottom: "1rem" }}>
+            <img className="YT" src="https://imgur.com/tonaxAB.png" alt="" />
+            報表生成中，請稍等一至兩分鐘
+            <img className="YT" src="https://imgur.com/tonaxAB.png" alt="" />
+          </h3>
           <div className="loader"></div>
         </div>
       ) : (
-        <Markdown className="my-table-class" remarkPlugins={[remarkGfm, gfmTable]} rehypePlugins={[rehypeRaw]}>
+        <Markdown
+          className="my-table-class"
+          remarkPlugins={[remarkGfm, gfmTable]}
+          rehypePlugins={[rehypeRaw]}
+        >
           {markdown}
         </Markdown>
       )}
--- src/components/markdownBlockTest.jsx
+++ src/components/markdownBlockTest.jsx
@@ -8,52 +8,109 @@ import Stack from "react-bootstrap/Stack";
 import axios from "axios";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import {micromark} from 'micromark'
-import {gfmTable, gfmTableHtml} from 'micromark-extension-gfm-table'
+import { micromark } from 'micromark'
+import { gfmTable, gfmTableHtml } from 'micromark-extension-gfm-table'
 import "./loading.css";
+import { v1 as uuidv1 } from 'uuid';
 
 function MarkdownBlocks() {
   const [keyword, setKeyword] = useState("");
-  const [markdown, setMarkdown] = useState(`# 事件總結標題：柯文哲與侯友宜的合作問題引發網友熱議
-
-  | 事件觀點 | 留言對此觀點的看法 |
-  |---------|------------------|
-  | 柯文哲秉持開闊的格局，致力整合各方建議與政見，絕非國民黨口中的「串門子」，而是匯聚民間力量，達成政黨輪替的目標。 | - 連柯都要主流民意大聯盟了？ <br> - 人家都提了民調+民主初選 <br> - 柯提出來的辦法都細到連數字都有了，侯辦也沒說柯的民調方案或數字哪一塊，他們不滿要改，反觀柯對侯的民調質疑，侯辦沒一樣認真回應的 |
-  | 侯友宜願意接受「柯侯配」，但柯文哲要回覆是否同意，否則進入政黨協商。 | - 你是不是很怕柯侯配 <br> - 侯柯這兩個咖洨真的想談 早就同桌坐下來談了啦 <br> - 侯還在堅持不可行沒科學可信度的民主初選根本冥頑不靈 |
-  | 侯友宜要求柯文哲回覆是否合作，否則進入政黨協商。 | - 柯郭是最佳解，候去旁邊玩沙 <br> - 侯真的被大家看衰，沒救了 <br> - 侯真的好吠 |
-  | 侯友宜願意接受「柯侯配」，但柯文哲要回覆是否同意，否則進入政黨協商。 | - 侯的說法就像是告訴柯，你吃屎我就當副手 <br> - 在一個對方不可能接受的前題下，才願意當副 <br> - 侯腦想要用條件限制柯，真的科科笑 |
-  
-  #### 總結：根據留言的分析，網友對於柯文哲與侯友宜的合作問題持有不同的看法。有些人認為柯文哲的做法是為了匯聚民間力量，達成政黨輪替的目標，並且提出了具體的辦法和數字，對於侯友宜的質疑感到不滿。另一方面，也有人認為侯友宜願意接受「柯侯配」，但柯文哲要回覆是否同意，否則進入政黨協商的態度是在限制柯文哲的選擇，並且對侯友宜的做法感到不滿。綜合來看，網友對於柯文哲與侯友宜的合作問題持有不同的觀點，並且對於侯友宜的做法有所質疑。`);
+  const [markdown, setMarkdown] = useState("");
   let today = new Date();
   const [dateRange, setDateRange] = useState([new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000), new Date(today.getTime() - 1 * 24 * 60 * 60 * 1000)]);
   const [startDate, endDate] = dateRange;
+  let timer = 0;
 
   const handleInputChange = (e) => {
     setKeyword(e.target.value);
   };
 
+  // Count for delay request
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  // Polling response from GPT's report
+  async function pollingData(url) {
+    timer += 1
+    if (timer == 1) {
+      await delay(60000); // 60000
+      console.log("waiting done.")
+    }
+    console.log("in pollingData, check timer.")
+    console.log("timer is ", timer)
+    if (timer > 2) {
+      return
+    } else {
+      try {
+        console.log("call check.")
+        const response = await axios.get(url);
+        console.log('Response:', response.status, response.data);
+        return response;
+      } catch (error) {
+        // If received 404. Retrying in 10 seconds
+        if (error.response && error.response.status === 404) {
+          console.log('Received 404. Retrying in 10 seconds...');
+          await delay(10000); // 10000
+          return pollingData(url);
+        } else {
+          // Catch other error
+          console.error('Error:', error);
+          setMarkdown("# 發生錯誤")
+          throw error;
+        }
+      }
+    }
+  }
+
+
   function submit() {
-    console.log("Keyword:", keyword);
-    let url = "http://140.120.182.177:11251/analyze/" + keyword + ("?tag=%E6%96%B0%E8%81%9E&K=5&size=10000&start=" + startDate.getTime().toString().slice(0, -3) + "&end=" + endDate.getTime().toString().slice(0, -3))
+    const uuidOfSession = uuidv1();
+    console.log("Keyword is :", keyword);
+    console.log("UUID is :", uuidOfSession);
+    // let url = "https://potato-elfbackend.nlpnchu.org/analyze/" + keyword + "?uuidOfSession=" + uuidOfSession + "&tag=%E6%96%B0%E8%81%9E&K=5&size=10000&start=" + startDate.getTime().toString().slice(0, -3) + "&end=" + endDate.getTime().toString().slice(0, -3)
+    let url = "https://potato-elfbackend.nlpnchu.org/analyze/" + keyword + "?uuidOfSession=" + uuidOfSession + "&tag=%E6%96%B0%E8%81%9E&K=5&size=10000&start=" + startDate.getTime().toString().slice(0, -3) + "&end=" + endDate.getTime().toString().slice(0, -3)
     setMarkdown("loading");
-    // console.log(url);
-    // console.log(startDate.getTime().toString().slice(0, -3));
-    // console.log(endDate.getTime().toString().slice(0, -3));
-    axios
-      .get(url)
-      .then((res) => {
-        console.log(res.data)
-        setMarkdown(res.data);
-      })
-      .catch((err) => {
-        console.log("error: ", err);
-        setMarkdown("# 發生錯誤")
-      });
+
+    if (keyword == "test_for_NCHU_NLP_LAB") {
+      axios.post(url)
+        .then((res) => {
+          setMarkdown(res.data);
+        })
+        .catch(err => setMarkdown("# Server Error."))
+    } else {
+      // Don't wait response. Use polling.
+      axios.post(url)
+        .then((res) => {
+          setMarkdown(res.data);
+        })
+        .catch(err => setMarkdown("# Server Error."))
+      console.log("Wait for 10 seconds.")
+      // setTimeout(() => {
+      //   // pollingData('https://potato-elfbackend.nlpnchu.org/check/' + uuidOfSession)
+      //   pollingData('http://127.0.0.1:8000/check/' + uuidOfSession)
+      //     .then(result => {
+      //       console.log("Get response by ", uuidOfSession);
+      //       setMarkdown(result.data);
+      //     })
+      //     .catch(err => {
+      //       console.log("error: ", err);
+      //       if (timer > 2) {
+      //         setMarkdown("# GPT Timeout.");
+      //       } else {
+      //         if(err.response.status === 524){
+      //           setMarkdown("# 524")
+      //         }
+      //       }
+      //     });
+      // }, 10000);
+    }
+
   }
 
   return (
     <Stack gap={3}>
-      <Stack direction="horizontal" gap={3}>
+      <Stack direction="horizontal" gap={1}>
         <Form.Control
           id="keywordTextArea"
           className="me-auto"
@@ -76,7 +133,11 @@ function MarkdownBlocks() {
       </Stack>
 
       {markdown == "loading" ? (
-        <div className="loader"></div>
+        <div className="loader-container">
+
+          <h3 style={{ textAlign: 'center', marginBottom: '1rem' }}><img className="YT" src="https://imgur.com/tonaxAB.png" alt="" />報表生成中，請稍等一至兩分鐘<img className="YT" src="https://imgur.com/tonaxAB.png" alt="" /></h3>
+          <div className="loader"><img className="YTimg" src="https://imgur.com/tonaxAB.png" alt="" /></div>
+        </div>
       ) : (
         <Markdown className="my-table-class" remarkPlugins={[remarkGfm, gfmTable]} rehypePlugins={[rehypeRaw]}>
           {markdown}
